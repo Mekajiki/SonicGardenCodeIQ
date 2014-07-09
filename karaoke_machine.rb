40ce119c36cb06ff0eeda33cfcaa3be9c65dafb5
Yuichiro NAKAGAWA <ii.hsif.drows@gmail.com>
2014-07-09 17:05:48 +0900

    use gsub and regular expression

--- karaoke_machine.rb
+++ karaoke_machine.rb
@@ -4,36 +4,11 @@ class KaraokeMachine
   end
 
   def transpose(ammount)
-    convert_table = CODES.zip(CODES.rotate(ammount)).to_h
-
-    symbols.map do |symbol|
-      if code_num = CODES.index(symbol)
-        convert_table[symbol]
-      else
-        symbol
-      end
-    end
-    .join
+    convert_table = KEYS.zip(KEYS.rotate(ammount)).to_h
+    @melody.gsub(KEY_REGEXP, convert_table)
   end
 
   private
-  CODES = %w(A A# B C C# D D# E F F# G G#)
-
-  def symbols
-    if @symbols
-      @symbols
-    else
-      @symbols = []
-
-      @melody.split('').each do |symbol|
-        if symbol == '#'
-          @symbols[-1] += symbol
-        else
-          @symbols.push symbol
-        end
-      end
-
-      @symbols
-    end
-  end
+  KEYS = %w(A A# B C C# D D# E F F# G G#)
+  KEY_REGEXP = /[A-G]#?/
 end
